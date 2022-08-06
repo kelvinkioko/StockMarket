b8eab45a10b5c220bf320b187794508b13a5e28d
Kelvin Kioko <kelvinmkioko@gmail.com>
2022-08-06 11:05:34 +0300

    [Reset] another Clean commit

--- app/src/main/java/compose/stockmarket/data/mapper/DummyInfoMapper.kt
+++ app/src/main/java/compose/stockmarket/data/mapper/DummyInfoMapper.kt
@@ -3,6 +3,8 @@ package compose.stockmarket.data.mapper
 import compose.stockmarket.data.remote.dto.CompanyInfoDto
 import compose.stockmarket.domain.model.CompanyInfoModel
 
+// This is another clean commit
+
 fun CompanyInfoDto.toDummyInfoModel(): CompanyInfoModel {
     return CompanyInfoModel(
         symbol = symbol ?: "",
